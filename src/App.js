44330562e450420d52fc1495fb706b6eb5633867
Lyutik13 <lyutikov.viktor.96@mail.ru>
2023-08-27 23:24:31 +0300

    box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Card from './components/Card/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
@@ -6,65 +7,75 @@ import Drawer from './components/Drawer'
 // import arr from './bd'
 
 function App() {
-	const [items, setItems] = React.useState([])
-	const [cartItems, setCartItems] = React.useState([])
-	const [searchValue, setSearchValue] = React.useState('')
-	const [cartOpened, setCartOpened] = React.useState(false)
+  const [items, setItems] = React.useState([])
+  const [cartItems, setCartItems] = React.useState([])
+  const [searchValue, setSearchValue] = React.useState('')
+  const [cartOpened, setCartOpened] = React.useState(false)
 
-	React.useEffect(() => {
-		fetch('https://64e20681ab00373588189d07.mockapi.io/items')
-			.then((res) => {
-				return res.json()
-			})
-			.then((json) => {
-				setItems(json)
-			})
-	}, [])
+  React.useEffect(() => {
+    // fetch('https://64e20681ab00373588189d07.mockapi.io/items')
+    //   .then((res) => {
+    //     return res.json()
+    //   })
+    //   .then((json) => {
+    //     setItems(json)
+    //   })
 
-	const onAddToCart = (obj) => {
-		setCartItems((prev) => [...prev, obj])
-	}
+      axios.get('https://64e20681ab00373588189d07.mockapi.io/items').then(res => {
+        setItems(res.data)
+      })
+      axios.get('https://64e20681ab00373588189d07.mockapi.io/cart').then(res => {
+        setCartItems(res.data)
+      })
+  }, [])
 
-	console.log(cartItems)
+  const onAddToCart = (obj) => {
+    axios.post('https://64e20681ab00373588189d07.mockapi.io/cart', obj)
+    setCartItems((prev) => [...prev, obj])
+  }
 
-	const onChangeSearchInput = (event) => {
-		console.log(event.target.value)
-		// setSearchValue()
-	}
+  const onRemuveItem = (id) => {
+    axios.delete(`https://64e20681ab00373588189d07.mockapi.io/cart/${id}`)
+    console.log(id)
+    setCartItems((prev) => prev.filter(item => item.id !== id))
+  }
 
-	return (
-		<div className="wrapper clear">
-			{/* {cartOpened ? <Drawer onClouseBasket={() => setCartOpened(false)} /> : null} */}
-			{cartOpened && (
-				<Drawer items={cartItems} onClouseBasket={() => setCartOpened(false)} />
-			)}
-			<Header onClickCard={() => setCartOpened(true)} />
+  return (
+    <div className="wrapper clear">
+      {/* {cartOpened ? <Drawer onClouseBasket={() => setCartOpened(false)} /> : null} */}
+      {cartOpened && (
+        <Drawer items={cartItems} onClouseBasket={() => setCartOpened(false)} onRemuve ={onRemuveItem}/>
+      )}
+      <Header onClickCard={() => setCartOpened(true)} />
 
-			<div className="content clear">
-				<div className="search">
-					<h1>Все кроссовки</h1>
-					<div className="search-block">
-						<img width={15} height={15} src="img/search.svg" alt="search" />
-						<input onChange={onChangeSearchInput} placeholder="Поиск..." />
-					</div>
-				</div>
-				<div className="cardWrapper">
-					{items.map((item, index) => (
-						<Card
-							key={index}
-							name={item.name}
-							price={item.price}
-							imageUrl={item.imageUrl}
-							// onFavorite={() => console.log('love')}
-							onPlus={onAddToCart}
-						/>
-					))}
-				</div>
-			</div>
-		</div>
-	)
+      <div className="content clear">
+        <div className="search">
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
+          <div className="search-block">
+            <img className='imgSearch' width={15} height={15} src="img/search.svg" alt="search" />
+            <input onChange={(event) => setSearchValue(event.target.value)} value={searchValue} placeholder="Поиск..." />
+            {searchValue && <img onClick={() => setSearchValue('')} className="btnClouseSearch" src="img/btn/btnClouseActive.svg" alt="Clear" />}
+          </div>
+        </div>
+        <div className="cardWrapper">
+          {items
+          .filter(item => item.name.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((item, index) => (
+            <Card
+              key={index}
+              name={item.name}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              // onFavorite={() => console.log('love')}
+              onPlus={onAddToCart}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
 
-//  0 28 #5
+// #5 1 14 30
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,58 +1,65 @@
-function Drawer({ onClouseBasket, items = [] }) {
-	return (
-		<div className="overlay">
-			<div className="drawer">
-				<h2>
-					Корзина
-					<img
-						onClick={onClouseBasket}
-						className="btnClouse"
-						src="img/btn/btnClouseActive.svg"
-						alt="btnClouse"
-					/>
-				</h2>
+function Drawer({ onClouseBasket, items = [], onRemuve }) {
+  return (
+    <div className="overlay">
+      <div className="drawer">
+        <h2>
+          Корзина
+          <img
+            onClick={onClouseBasket}
+            className="btnClouse"
+            src="img/btn/btnClouseActive.svg"
+            alt="btnClouse"
+          />
+        </h2>
 
-				<div className="cartItems">
-					{items.map((obj) => (
-						<div className="cartItem">
-							<img
-								width={70}
-								height={70}
-								src={obj.imageUrl}
-								alt="sneakers2"
-							/>
-							<div>
-								<p>{obj.name}</p>
-								<b>{obj.price} руб.</b>
-							</div>
-							<img
-								className="btnClouse"
-								src="img/btn/btnClouseActive.svg"
-								alt="btnClouse"
-							/>
-						</div>
-					))}
-				</div>
-
-				<ul className="cartFooter">
-					<li>
-						<span>Итого:</span>
-						<div></div>
-						<b>21 498 руб. </b>
-					</li>
-					<li>
-						<span>Налог 5%:</span>
-						<div></div>
-						<b>1074 руб. </b>
-					</li>
-				</ul>
-				<button className="greenBtn">
-					Оформить заказ
-					<img src="img/btn/btnImg.svg" alt="" />
-				</button>
-			</div>
-		</div>
-	)
+        {items.length > 0 ?
+          <div>
+            <div className="cartItems">
+              {items.map((obj) => (
+                <div className="cartItem">
+                  <img width={70} height={70} src={obj.imageUrl} alt="sneakers2" />
+                  <div>
+                    <p>{obj.name}</p>
+                    <b>{obj.price} руб.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemuve(obj.id)}
+                    className="btnClouse"
+                    src="img/btn/btnClouseActive.svg"
+                    alt="btnClouse"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="cartFooterBlock">
+              <ul className="cartFooter">
+                <li>
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>21 498 руб. </b>
+                </li>
+                <li>
+                  <span>Налог 5%:</span>
+                  <div></div>
+                  <b>1074 руб. </b>
+                </li>
+              </ul>
+              <button className="greenBtn">
+                Оформить заказ
+                <img src="img/btn/btnImg.svg" alt="" />
+              </button>
+            </div>
+          </div> :
+          <div className="cartEmpty">
+            <img src="img/box.jpg" alt="box" />
+            <h2>Корзина пустая</h2>
+            <p>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
+            <button onClick={onClouseBasket} className="greenBtn greenBtnLeftImg"><img src="img/btn/btnImgLeft.svg" alt="btnImgLeft" />Вернуться назад</button>
+          </div>
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Drawer
